2d0fc9e3aedd175827d44ba845ddf32375748c00
Fahim Sahriar <fahim.sahriar@sjinnovation.com>
2024-03-14 16:30:50 +0600

    liite update of gpt_functions from office

--- server/gpt_functions/createEvent.js
+++ server/gpt_functions/createEvent.js
@@ -1,16 +1,17 @@
-const { google } = require('googleapis');
+// const { google } = require('googleapis');
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-let REFRESH_TOKEN = "1//0gt7U_t7xFkJnCgYIARAAGBASNgF-L9IrzcJq74alasYJu8IybqgoxAJXvIMLp3MVBFXo1SSDH80rf_c1LgzOl_0obrpTWwRk1A";
+// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+// const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+// let REFRESH_TOKEN = "1//0gt7U_t7xFkJnCgYIARAAGBASNgF-L9IrzcJq74alasYJu8IybqgoxAJXvIMLp3MVBFXo1SSDH80rf_c1LgzOl_0obrpTWwRk1A";
 
-const oauth2Client = new google.auth.OAuth2(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    'http://localhost:3000'
-);
+// const oauth2Client = new google.auth.OAuth2(
+//     GOOGLE_CLIENT_ID,
+//     GOOGLE_CLIENT_SECRET,
+//     'http://localhost:3000'
+// );
 
 const createEvent = async (event) => {
+  
   try {
     console.log(event);
     return JSON.stringify({
